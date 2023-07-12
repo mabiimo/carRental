fa5a3fec15733216a70531c0741ea82fe4ca778e
3IA03_ Muhammad Aryo Bimo <muharyo1995@gmail.com>
2023-07-12 17:10:31 +0700

    ignore errors

---
+++ components/CustomFilters.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const CustomFilters = () => {
+  return <div>Custom Filters</div>;
+};
+
+export default CustomFilters;
---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilters from "./CustomFilters";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+
+export { Hero, CarDetails, CarCard, CustomButton, Navbar, Footer, SearchBar, CustomFilters, SearchManufacturer };
